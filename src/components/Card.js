dfe5c1966c9e98025a341e118d39bbd9ff362713
MusliHyseni <muslihyseni111@gmail.com>
2024-01-29 15:55:48 +0100

    Finished home page

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,25 @@
 import React from 'react'
 
-function Card() {
+function Card({data}) {
   return (
-    <div ></div>
+    
+    <div class="card m-2 border-info">
+    <h6 class="card-header d-flex align-items-center justify-content-center">{data.company_name.toUpperCase()}</h6>
+    <div class="card-body">
+        <h6 class="card-title align-items-center fs-5 mb-5">{data.title}</h6>
+        <div className='row justify-content-center card-text tagHolder'>
+            {data.tags && data.tags.map(tag => (
+                <span class="badge m-1 col-sm-10  p-3">{tag}</span>
+            ))}
+        </div>
+    </div>
+    <a href={data.url} class="btn card-footer">Read more</a>
+</div>
+        
+       
+        
+        
+    
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,14 +8,18 @@ function Home() {
   useEffect(() => {
     fetch('https://www.arbeitnow.com/api/job-board-api')
     .then(response => response.json())
-    .then(data => setJobs(data.data.slice(0, 17)))
+    .then(data => setJobs(data.data.slice(0, 9)))
   }, [])
 
   return (
-    <div>
-      {jobs && jobs.map((job, index) => {
-        // <Card key={index} data={job}/>
-      })}
+    <div className='row justify-content-center'>
+      <img src={poster} className='mb-5'/>
+      <h1 className='fw-bold mb-5 '>Your dream career starts with us!</h1>
+      {jobs && jobs.map((job, index) => (
+        <div className='col-3 m-1' key={index}>
+          <Card data={job}/>
+        </div>
+      ))}
     </div>
   )
 }
