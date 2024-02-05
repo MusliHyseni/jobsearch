a9f9384a95e3f181e916c03ba43ca74763fc2b4e
MusliHyseni <muslihyseni111@gmail.com>
2024-02-05 15:34:20 +0100

    .

--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Jobs() {
+  const [jobs, setJobs] = useState([]);
+  const [categories, setCategories] = useState([])
+  
+  useEffect(() => {
+    fetch('https://www.arbeitnow.com/api/job-board-api')
+    .then(response => response.json())
+    .then(data => setJobs(data.data));
+  }, [])
+
+  const c_array = jobs.map(job => job.tags[0]);
+  // Remove duplicates from the tags
+  visited = [];
+  for (let tag of c_array) {
+    if (!visited.includes(tag)) {
+      visited.push(tag)
+    }
+  }
+  
+  
+
   return (
     <div>Jobs</div>
   )
