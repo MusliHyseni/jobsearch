39646f3cd17d8ce3682dce122b05a360dc128e9d
MusliHyseni <muslihyseni111@gmail.com>
2024-02-10 06:12:40 +0100

    Fixed route

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/jobs' element={<Jobs/>}/>
+        <Route path='/jobs/:job-tag/apply'/>
         <Route path='/myjobs' element={<MyJobs/>}/>
       </Routes>
     <Footer/>
