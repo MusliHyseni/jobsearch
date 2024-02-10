b715ec91c351f5765e13217382eb625decaceb8d
MusliHyseni <muslihyseni111@gmail.com>
2024-02-10 06:13:35 +0100

    Added filter button

--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from 'react'
+import Dropdown from 'react-bootstrap/Dropdown';
+import Form from 'react-bootstrap/Form';
+import {Container, Button} from 'react-bootstrap';
+
 import DataCard from '../components/Card';
-import {Container } from 'react-bootstrap';
 import ScrollMenu from '../components/ScrollMenu';
+
 
 function Jobs() {
   const [jobs, setJobs] = useState([]);
+  const [prevJobs, setPrevJobs] = useState([]);
+  const [isFiltered, setIsFiltered] = useState(false);
   
   useEffect(() => {
     fetch('https://www.arbeitnow.com/api/job-board-api')
@@ -21,19 +27,70 @@ function Jobs() {
     }
   }
 
+  console.log(jobs[0]);
 
+  const handleFilter = e => {
+    e.preventDefault();
+    setPrevJobs(jobs);
+    setJobs(jobs.filter(job => (job.tags[0] == e.target.getAttribute('id'))));
+    setIsFiltered(true);
+  }
 
+  const handleRemoveFilter = e => {
+    setJobs(prevJobs);
+  }
+
+  const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+    <a
+      href=""
+      ref={ref}
+      onClick={(e) => {
+        e.preventDefault();
+        onClick(e);
+      }}
+    >
+      {children}
+      &#x25bc;
+    </a>
+  ));
+
+ 
   
 
+
   return (
-    <Container className=''>
-    {visited && visited.map((name, index) => (
-      <Container key={index} className=''>
-        <h3>{name}</h3>
-        <ScrollMenu data={jobs.filter(job => (job.tags[0] == name))}/>
-      </Container>
-    ))}
-  </Container>
+    <>
+        <Dropdown>
+          <Dropdown.Toggle as={CustomToggle} className='filter-menu' id="dropdown-custom-components">
+            Filter by category
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            {visited.map((item, index) => (
+              <Dropdown.Item eventKey={index}>
+                <Button className='bg-white text-black' id={item} onClick={handleFilter}>{item}</Button>
+              </Dropdown.Item>
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
+        {isFiltered &&  
+              <Button className="btn-danger col" onClick={handleRemoveFilter}>
+                  Remove filters
+              </Button>}
+      <Container xs={12} className='container-fluid myContainer'>
+      {visited && visited.map((name, index) => (
+        
+        ((jobs.filter(job => (job.tags[0] == name))).length > 2 && 
+          <> 
+            <Container key={index} className='w-100 m-0 p-0 otherContainer'>
+              <h3 className='category-title fw-bold'>{name}</h3>
+              <ScrollMenu data={jobs.filter(job => (job.tags[0] == name))}/>
+            </Container>
+          </>)
+        
+      ))}
+    </Container>
+  </>
   )
 }
 
